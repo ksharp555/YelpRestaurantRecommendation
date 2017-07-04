0a179068c611b0da3f039edf74a1bbc22fb7d5ad
ksharp555 <jiahechang01@gmail.com>
2017-07-04 12:28:37 -0700

    Initial Commit

---
+++ YelpStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YelpStudy
+//
+//  Created by Jiahe Chang on 7/4/17.
+//  Copyright © 2017 Jiahe Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YelpStudy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YelpStudy
+//
+//  Created by Jiahe Chang on 7/4/17.
+//  Copyright © 2017 Jiahe Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
